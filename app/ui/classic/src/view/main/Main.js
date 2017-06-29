4d28c7705efe4f16ed3cd6a4067b0c66d8d0c0f7
dashsant <Santi.Dash@gmail.com>
2017-06-29 15:22:38 -0400

    made main panel as border

--- app/ui/classic/src/view/main/Main.js
+++ app/ui/classic/src/view/main/Main.js
@@ -1,104 +1,50 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting automatically applies the "viewport"
- * plugin causing this view to become the body element (i.e., the viewport).
- *
- * TODO - Replace this content of this view to suite the needs of your application.
- */
-Ext.define('newsscraper.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
 
+
+Ext.define('newsscraper.view.main.Main', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'app-main',
+    layout: 'border',
+    height:"100%",
+    width:"100%",
+    header:false,
     requires: [
         'Ext.plugin.Viewport',
         'Ext.window.MessageBox',
 
         'newsscraper.view.main.MainController',
-        'newsscraper.view.main.MainModel',
-        'newsscraper.view.main.List'
+        'newsscraper.view.main.MainModel'
     ],
 
     controller: 'main',
     viewModel: 'main',
-
-    ui: 'navigation',
-
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
+    items: [
+     {
+        header: false,
+	region: 'south',     // position for region
+        xtype: 'panel',
+	split:true,
+	splitterResize:false,
+	height:"80%",
+        //layout: 'fit',
+        margin: '0 5 5 5'
     },
+    {
+        title: 'Center Region',
+        region: 'center',     // center region is required, no width/height specified
+        xtype: 'panel',
+        height: 100,
+        margin: '5 5 0 0',
+	bbar: [ { xtype: 'button', text: 'Most Recent'} ,  
+		{ xtype: 'button', text: 'Last 1 weeks'} ,  
+		{ xtype: 'button', text: 'Last 2 weeks'} ,
+		{ xtype: 'button', text: 'Last 3 weeks'} ,
+		{ xtype: 'button', text: 'Last 4 weeks'} ,
+		{ xtype: 'button', text: 'Custom'}
+		],
+    }
+    ]
 
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            plugins: 'responsive',
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
-    },
-
-    items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
-        }]
-    }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }]
 });
+
+
+
